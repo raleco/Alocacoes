929a3393d2a5f318efeb280a1e1652e4e999b18a
alamaral <alamaral@deloitte.com>
2021-12-27 08:51:52 -0300

    commit

---
+++ force-app/main/default/aura/EmployeeResume/EmployeeResumeController.js
@@ -0,0 +1,11 @@
+({
+    doInit : function(component, event, helper) {
+       /* var funcId = component.get("v.recordId");
+        var urlEvent = $A.get("e.force:navigateToURL");
+        urlEvent.setParams({
+            "url": "/lightning/r/" + funcId + "/related/Ferias__r/view"
+            
+        });
+        urlEvent.fire(); */
+    } 
+})
